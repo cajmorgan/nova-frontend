d4f6e775424828d26b3888625d3824a8dd4b094a
Caj <caj@cajmorgan>
2021-10-28 10:22:58 +0200

    upd

--- Generator.js
+++ Generator.js
@@ -1,10 +1,38 @@
 class Generator {
   constructor() {
-
+    this.rules = {
+      typeExpected: true,
+      valueExpected: false,
+      checkNextTypeIsValue: false
+    }
   }
   
-  checkGrammar() {
-    return 'something';
+  checkGrammar(currentToken) {
+    /** Grammars
+     * token_type ex. div section form button
+     * token_property
+     * token_value 
+     */
+    const tags = ["a","abbr","acronym","address","applet","area","article","aside","audio","b","base","basefont","bb","bdo","big","blockquote","body","br /","button","canvas","caption","center","cite","code","col","colgroup","command","datagrid","datalist","dd","del","details","dfn","dialog","dir","div","dl","dt","em","embed","eventsource","fieldset","figcaption","figure","font","footer","form","frame","frameset","h1 to h6","head","header","hgroup","hr /","html","i","iframe","img","input","ins","isindex","kbd","keygen","label","legend","li","link","map","mark","menu","meta","meter","nav","noframes","noscript","object","ol","optgroup","option","output","p","param","pre","progress","q","rp","rt","ruby","s","samp","script","section","select","small","source","span","strike","strong","style","sub","sup","table","tbody","td","textarea","tfoot","th","thead","time","title","tr","track","tt","u","ul","var","video","wbr"]
+    let returnType = '';
+
+    if (this.rules.typeExpected) {
+      if (tags.includes(currentToken)) {
+        returnType = 'token_type';
+      }
+    }
+
+    if (this.rules.checkNextTypeIsValue) {
+      if (currentToken[0] === '\'' && currentToken[currentToken.length - 1] === '\'') {
+        returnType = 'token_value';
+      }
+    }
+
+    return returnType;
+  }
+
+  createElementsFromTokens() {
+
   }
 
   createTree(input) {
@@ -20,20 +48,16 @@ class Generator {
       } 
 
       //Rule: Value Expected -- If ':' is found, next grammarcheck type needs to be 'value' else throw error
-      const rules = {
-        valueExpected: false
-      }
       const tokens = [];
       let currentToken = '';
-      for (let lineIndex = indentation; lineIndex < line.length; lineIndex++) {
+      for (let lineIndex = indentation; lineIndex <= line.length; lineIndex++) {
         let lastChar = ''
-        if (line[lineIndex] !== ' ' && line[lineIndex]) {
-          if (rules.valueExpected && line[lineIndex] !== '\'') 
-            throw new Error('Missing \' after :');
-          rules.valueExpected = false;
-          
+        if ((line[lineIndex] !== ' ' && lineIndex !== line.length) && line[lineIndex]) {
+          if (this.rules.valueExpected && line[lineIndex] !== '\'') 
+            throw new Error('Missing \' after :');         
+
           if (line[lineIndex] === ':') {
-            rules.valueExpected = true;
+            this.rules.valueExpected = true;
             continue;
           }
 
@@ -42,12 +66,26 @@ class Generator {
         } else {
           //Validate token
           const type = this.checkGrammar(currentToken)
+
+          if (this.rules.typeExpected && type !== 'token_type') 
+            throw new Error("type expected as first token! ex. div");
+          this.rules.typeExpected = false;
+
+          if (this.rules.checkNextTypeIsValue && type !== 'token_value') 
+            throw new Error("'value' expected after after token_property")
+          this.rules.checkNextTypeIsValue = false;
+
+          if (this.rules.valueExpected) {
+            this.rules.checkNextTypeIsValue = true;
+            this.rules.valueExpected = false;
+          }
+
           //If type === htmlChanger something, validate with lastchar
-          tokens.push({token: currentToken, type });
+          tokens.push({ token: currentToken, type });
           currentToken = '';
         }
         
-      
+        //Create element from tokens
       }
       console.log(tokens);
       
@@ -58,6 +96,14 @@ class Generator {
   } 
 }
 
+const generator = new Generator()
+generator.createTree(`
+  div className: 'helloDiv'
+    section
+      h1 className: 'helo' innerText: 'hejsan'
+    article
+  div
+`)
 
 
 // Tree creation from pseudo html input from generator below
@@ -102,11 +148,3 @@ const exampleObj = [{
 ]
 
 
-const generator = new Generator()
-generator.createTree(`
-  div className: 'helloDiv'
-    section
-      h1 className: 'helo' innerText: 'hejsan'
-    article
-  div
-`)
--- test.js
+++ test.js
@@ -1,20 +1,26 @@
 
 function createHeader() {
-  const header = new Element('header', root, { className: 'header' });
-    const header__div = new Element('div', header, { className: 'header__div' });
-      const h1 = new Element('h1', header__div, { className: 'header__div--h1', innerText: "Welcome To New Framework!" });
-      const h2 = new Element('h2', header__div, { className: 'header__div--h2', innerText: "hi me"});
-      const textinput = new Element('input', header__div, { id: 'text-input', type: 'text', value: 5 });
-      let counter = 0;
-  textinput.addEventListener('click', () => {
-    h2.toggleNode();
+  // for (const tag in window) {
+  //   console.log(tag);
+  // }
+
+ 
+  
+  // const header = new Element('header', root, { className: 'header' });
+  //   const header__div = new Element('div', header, { className: 'header__div' });
+  //     const h1 = new Element('h1', header__div, { className: 'header__div--h1', innerText: "Welcome To New Framework!" });
+  //     const h2 = new Element('h2', header__div, { className: 'header__div--h2', innerText: "hi me"});
+  //     const textinput = new Element('input', header__div, { id: 'text-input', type: 'text', value: 5 });
+  //     let counter = 0;
+  // textinput.addEventListener('click', () => {
+  //   h2.toggleNode();
     
-    counter++;
-    // const value = textinput.value * 2;
-    // textinput.updateNode({ value });
-    // h2.updateNode({ innerText: value });
-    // h2.addStyle('color', 'red');
-  })
+  //   counter++;
+  //   // const value = textinput.value * 2;
+  //   // textinput.updateNode({ value });
+  //   // h2.updateNode({ innerText: value });
+  //   // h2.addStyle('color', 'red');
+  // })
 }
 
 createHeader();
