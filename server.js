35b55832fea47b115c6381d0d47e8b4c9b25491c
cajmorgan <caj@cajmorgan.com>
2022-03-05 08:16:13 +0100

    Static generation mostly done

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const path = require('path');
+const { StaticCompiler } = require('./index');
 
 const app = express();
 app.use(express.static('build'));
+
+(async () => {
+  const compiler = new StaticCompiler([], {}, '', true);
+  await compiler.fullBuild();
+
+  // console.log(compiler)
+})()
 
 app.get('*', (req, res) => {
   res.send('helo')
