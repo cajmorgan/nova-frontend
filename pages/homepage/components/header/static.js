cc4d72438a41b09d0aa75c3ac4b06b18828c4f80
cajmorgan <caj@cajmorgan.com>
2022-03-16 21:34:32 +0100

    setprops functionality for components

--- pages/homepage/components/header/static.js
+++ pages/homepage/components/header/static.js
@@ -2,15 +2,24 @@ const { StaticGenerator } = require('../../../../nova')
 
 const gen = new StaticGenerator();
 
+const arrTest = [{ title: 'hello', desc: 'nicenice' }, { title: 'hiho', desc: 'fyfan' }];
+
+let arrTestString = '';
+arrTest.forEach(one => {
+  const comp = gen.createTree(`
+    div
+      h1 innerText: '{{title}}'
+      p innerText: '{{desc}}'
+  end`)
+
+  comp.setProps({ title: one.title, desc: one.desc });
+  arrTestString += comp.HTMLString;
+})
+
 const header = gen.createTree(`
-  div className: 'grandfather' id: 'awesome'
-    div className: 'mother'
-      h1 innerText: 'hello'
-      h2 innerText: 'well'
-      p innerText: 'yo'
-    div className: 'father'
-      div
-        h2 innerText: 'nesting'
+  div className: 'wrapper' innerText: '{{arrTest}}'
 end`)
+
+header.setProps({ arrTest: arrTestString });
 
 module.exports = header;
